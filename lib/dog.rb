534456a3cd1b05d3cd81d0001a8ff3ae02352e55
Mike Nanto <mike@thecowboymechanic.com>
2021-01-24 05:32:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,5 +6,20 @@ class Dog
     @this_dogs_name
   end
   
+  def name=(new_name)
+    @this_dogs_name = new_name
+  end
+  
+  def breed =(breed)
+    @this_dogs_breed = breed
+  end
+  
+  def breed
+    @this_dogs_breed
+  
+  def breed=(new_breed)
+    @this_dogs_breed = new_breed
+  end
+  
 end
   
